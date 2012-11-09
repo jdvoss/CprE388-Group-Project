1b398c75d9181fa46e43e286584cded673a5962e
Jamison Voss <jamisonvoss@Jamisons-MacBook-Pro.local>
2012-11-08 23:06:12 -0600

    Initial Commit

---
+++ Word Chain/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Word Chain
+//
+//  Created by Jamison Voss on 11/8/12.
+//  Copyright (c) 2012 Jamison Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Word Chain/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Word Chain
+//
+//  Created by Jamison Voss on 11/8/12.
+//  Copyright (c) 2012 Jamison Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
